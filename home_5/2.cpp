a599d99df8ea3caaf8c2bddab857b2a5dcf29f48
nickolas <lorddarthveider7@yandex.ru>
2016-04-23 11:18:01 +0300

    Added Heap. Data dived to AVLData and HeapData.

--- home_5/2.cpp
+++ home_5/2.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 #include <sstream>
 
-struct Data {
+struct AVLData {
     std::string name;
-    int val;
-    bool operator==(const Data &) const;
-    bool operator<(const Data &) const;
-    friend std::istream& operator>>(std::istream &, Data &);
-    friend std::ostream& operator<<(std::ostream &, const Data &);
+    int heap_pos;
+    bool operator<(const AVLData &) const;
+    bool operator==(const AVLData &) const;
+    friend std::istream& operator>>(std::istream &, AVLData &);
+    friend std::ostream& operator<<(std::ostream &, const AVLData &);
 };
 
-
 class AVL {
+    friend class HeapData;
     struct AVLnode {
         AVLnode *left, *right;
-        Data data;
+        AVLData data;
         int height;
-        AVLnode(const Data&);
+        AVLnode(const AVLData&);
         AVLnode *insert(AVLnode *);
         AVLnode* balance();
         void print(std::string &) const;
@@ -30,45 +30,70 @@ class AVL {
 public:
     AVL();
     ~AVL();
-    void insert(const Data &);
-    void change(const Data &);
-    const Data& find(const Data &) const;
+    void insert(const AVLData &);
+    void change(const AVLData &);
+    const AVLData& find(const AVLData &) const;
     std::string print() const;
+};
+
+struct HeapData {
+    int priority;
+    AVL::AVLnode* node;
+    HeapData();
+    ~HeapData();
+    bool operator<(const HeapData &) const;
+    bool operator==(const HeapData &) const;
 };
 
 class Heap {
     int capacity;
     int size;
-    Data* data;
-    void sift_down();
-    void sift_up();
+    HeapData* data;
+    void sift_down(int);
+    void sift_up(int);
     void extand();
 public:
     Heap();
     ~Heap();
-    Data get_max() const;
+    HeapData get_max() const;
     void pop_max();
-    int add(const Data &);
-    int increase_priority(const Data &);
+    void add(const HeapData &);
+    void increase_priority(int, const HeapData &);
     bool empty() const;
-    void print() const;
+    std::string print() const;
 };
 
 
-bool Data::operator==(const Data &data) const {
+bool AVLData::operator==(const AVLData &data) const {
     return name == data.name;
 }
-bool Data::operator<(const Data &data) const {
+bool AVLData::operator<(const AVLData &data) const {
     return name < data.name;
 }
-std::istream& operator>>(std::istream &in, Data &data) {
+std::istream& operator>>(std::istream &in, AVLData &data) {
     in >> data.name;
-    in >> data.val;
+    in >> data.heap_pos;
     return in;
 }
-std::ostream& operator<<(std::ostream &out, const Data &data) {
-    out << data.name << ' ' << data.val;
+std::ostream& operator<<(std::ostream &out, const AVLData &data) {
+    out << data.name << ' ' << data.heap_pos;
     return out;
+}
+
+HeapData::HeapData() {
+    node = NULL;
+    priority = 0;
+}
+HeapData::~HeapData() {
+    node = NULL;
+    priority = 0;
+}
+bool HeapData::operator<(const HeapData &d) const {
+    return priority < d.priority;
+}
+
+bool HeapData::operator==(const HeapData &d) const {
+    return priority == d.priority;
 }
 
 void AVL::delete_nodes(AVLnode *node) {
@@ -88,7 +113,7 @@ AVL::~AVL() {
     delete_nodes(root);
 }
 
-AVL::AVLnode::AVLnode(const Data &data) {
+AVL::AVLnode::AVLnode(const AVLData &data) {
     height = 1;
     right = left = NULL;
     this->data = data;
@@ -104,7 +129,7 @@ AVL::AVLnode* AVL::AVLnode::insert(AVLnode *node) {
     return balance();
 }
 
-void AVL::insert(const Data &data) {
+void AVL::insert(const AVLData &data) {
     AVLnode *new_node = new AVLnode(data);
     if (root == NULL) {
         root = new_node;
@@ -118,8 +143,6 @@ void AVL::AVLnode::print(std::string &output) const {
     if (left != NULL) {
         left->print(output);
     }
-    //std::cout << data << std::endl;
-    //output += data.data + '\n';
     std::stringstream istr;
     istr << data << '\n';
     output += istr.str();
@@ -185,7 +208,7 @@ AVL::AVLnode* AVL::AVLnode::balance() {
 }
 
 
-const Data& AVL::find(const Data &data) const {
+const AVLData& AVL::find(const AVLData &data) const {
     AVLnode *current = root;
     while (current != NULL && !(current->data == data)) {
         if (data < current->data) {
@@ -202,11 +225,95 @@ const Data& AVL::find(const Data &data) const {
 }
 
 
-void AVL::change(const Data &data) {
-    const Data &d = find(data);
-    const_cast<Data&>(d).val = data.val;
+void AVL::change(const AVLData &data) {
+    const AVLData &d = find(data);
+    const_cast<AVLData&>(d).heap_pos = data.heap_pos;
 }
 
+void Heap::extand() {
+    HeapData *new_data = new HeapData[capacity << 1];
+    std::copy(data, data + capacity, new_data);
+    delete[] data;
+    data = new_data;
+    capacity <<= 1;
+}
+
+void Heap::sift_up(int idx) {
+    while (idx > 1 && data[idx / 2] < data[idx]) {
+        std::swap(data[idx / 2], data[idx]);
+        if (data[idx].node != NULL) {
+            data[idx].node->data.heap_pos = idx;
+        }
+        if (data[idx / 2].node != NULL) {
+            data[idx / 2].node->data.heap_pos = idx / 2;
+        }
+        idx /= 2;
+    }
+}
+
+void Heap::sift_down(int idx) {
+    while (2 * idx < size) {
+        int idx_max = 2 * idx;
+        if (2 * idx + 1 < size && data[2 * idx] < data[2 * idx + 1]) {
+            idx_max = 2 * idx + 1;
+        }
+        if (data[idx] < data[idx_max]) {
+            std::swap(data[idx], data[idx_max]);
+            if (data[idx].node != NULL) {
+                data[idx].node->data.heap_pos = idx;
+            }
+            if (data[idx_max].node != NULL) {
+                data[idx_max].node->data.heap_pos = idx_max;
+            }
+            idx = idx_max;
+        }
+        else {
+            break;
+        }
+    }
+}
+Heap::Heap() {
+    capacity = 5;
+    size = 1;
+    data = new HeapData[capacity];
+}
+
+Heap::~Heap() {
+    delete[] data;
+}
+
+HeapData Heap::get_max() const {
+    return data[1];
+}
+void Heap::pop_max() {
+    std::swap(data[1], data[size - 1]);
+    --size;
+    sift_down(1);
+}
+void Heap::add(const HeapData &d) {
+    if (size == capacity) {
+        extand();
+    }
+    data[size++] = d;
+    sift_up(size - 1);
+}
+void Heap::increase_priority(int x, const HeapData &d) {
+    data[x].priority = d.priority;
+    sift_up(x);
+    sift_down(x);
+}
+bool Heap::empty() const {
+    return size == 1;
+}
+std::string Heap::print() const {
+    std::string out;
+    for (int i = 1; i < size; ++i) {
+        std::stringstream ss;
+        //ss << data[i] << '\n';
+        out += ss.str();
+    }
+    return out;
+}
 /*
 int main() {
     Data data;
--- home_5/tester_2.cpp
+++ home_5/tester_2.cpp
@@ -7,8 +7,8 @@
 
 #include "2.cpp"
 
-TEST(data, input) {
-    Data d;
+TEST(AVLData, input) {
+    AVLData d;
     std::ifstream in1("1.dat");
     std::ifstream in2("1.dat");
     
@@ -19,13 +19,13 @@ TEST(data, input) {
     in2 >> str >> val;
     in2.close();
     ASSERT_EQ(str, d.name);
-    ASSERT_EQ(val, d.val);
+    ASSERT_EQ(val, d.heap_pos);
 }
 
-TEST(data, output) {
-    Data d;
+TEST(AVLData, output) {
+    AVLData d;
     d.name = "Ivan";
-    d.val = 10;
+    d.heap_pos = 10;
     
     std::ofstream out("2.dat");
     out << d;
@@ -39,50 +39,36 @@ TEST(data, output) {
     ASSERT_EQ(10, val);
 }
 
-TEST(data, equal1) {
-    Data d1, d2;
+TEST(AVLData, equal) {
+    AVLData d1, d2;
     d1.name = "Nick";
     d2.name = "Ivan";
-    d1.val = 10;
-    d2.val = 40;
-    
+    d1.heap_pos = 10;
+    d2.heap_pos = 40;
     ASSERT_FALSE(d1 == d2);
-}
 
-TEST(data, equal2) {
-    Data d1, d2;
+    AVLData d3, d4;
+    d3.name = "Nick";
+    d4.name = "Nick";
+    d3.heap_pos = d4.heap_pos = 10;
+    ASSERT_TRUE(d3 == d4);
+
     d1.name = "Nick";
     d2.name = "Nick";
-    d1.val = d2.val = 10;
+    d1.heap_pos = 10;
+    d2.heap_pos = 12415;
+    ASSERT_TRUE(d1 == d2);
     
-    ASSERT_TRUE(d1 == d2);
-}
-
-TEST(data, equal3) {
-    Data d1, d2;
-    d1.name = "Nick";
-    d2.name = "Nick";
-    d1.val = 10;
-    d2.val = 12415;
-    ASSERT_TRUE(d1 == d2);
-}
-
-TEST(data, equal4) {
-    Data d1, d2;
     d1.name = "Nick";
     d2.name = "Ivan";
-    d1.val = d2.val = 10;
-    ASSERT_FALSE(d1 == d2);
-}
-
-TEST(data, equal5) {
-    Data d1, d2;
-    d1.name = "Nick";
-    d2.name = "Ivan";
-    d1.val = 15;
-    d2.val = 123;
+    d1.heap_pos = d2.heap_pos = 10;
     ASSERT_FALSE(d1 == d2);
     
+    d1.name = "Nick";
+    d2.name = "Ivan";
+    d1.heap_pos = 15;
+    d2.heap_pos = 123;
+    ASSERT_FALSE(d1 == d2);
     d2.name = "Nick";
     ASSERT_TRUE(d1 == d2);
     
@@ -90,40 +76,31 @@ TEST(data, equal5) {
     ASSERT_FALSE(d1 == d2);
 }
 
-TEST(data, less1) {
-    Data d1, d2;
+TEST(AVLData, less) {
+    AVLData d1, d2;
     d1.name = "Alex";
     d2.name = "Bogdan";
-    d1.val = 5;
-    d2.val = 10;
+    d1.heap_pos = 5;
+    d2.heap_pos = 10;
     ASSERT_TRUE(d1 < d2);
     ASSERT_FALSE(d2 < d1);
-}
-
-TEST(data, less2) {
-    Data d1, d2;
+    
     d1.name = "Alex";
     d2.name = "Alex";
-    d1.val = d2.val = 10;
+    d1.heap_pos = d2.heap_pos = 10;
     ASSERT_FALSE(d1 < d2);
     ASSERT_FALSE(d2 < d1);
-}
-
-TEST(data, less3) {
-    Data d1, d2;
+    
     d1.name = "Alex";
     d2.name = "Alex";
-    d1.val = 10;
-    d2.val = 20;
+    d1.heap_pos = 10;
+    d2.heap_pos = 20;
     ASSERT_FALSE(d1 < d2);
     ASSERT_FALSE(d2 < d1);
-}
-
-TEST(data, less4) {
-    Data d1, d2;
+    
     d1.name = "Gosha";
     d2.name = "Natasha";
-    d1.val = d2.val = 1123213;
+    d1.heap_pos = d2.heap_pos = 1123213;
     ASSERT_TRUE(d1 < d2);
     ASSERT_FALSE(d2 < d1);
 }
@@ -140,32 +117,28 @@ TEST(AVL, create) {
 TEST(AVL, insert) {
     AVL avl;
     
-    Data d;
+    AVLData d;
     d.name = "Aaaa";
-    d.val = 1;
+    d.heap_pos = 1;
     avl.insert(d);
-    
     ASSERT_EQ(std::string("Aaaa 1\n"), avl.print());
 
-    Data d2;
+    AVLData d2;
     d2.name = "Bbbbb";
-    d2.val = 2;
+    d2.heap_pos = 2;
     avl.insert(d2);
-    
     ASSERT_EQ(std::string("Aaaa 1\nBbbbb 2\n"), avl.print());
 
-    Data d3;
+    AVLData d3;
     d3.name = "Ccc";
-    d3.val = 3;
+    d3.heap_pos = 3;
     avl.insert(d3);
-
     ASSERT_EQ(std::string("Aaaa 1\nBbbbb 2\nCcc 3\n"), avl.print());
 
-    Data d4;
+    AVLData d4;
     d4.name = "A";
-    d4.val = 0;
+    d4.heap_pos = 0;
     avl.insert(d4);
-
     ASSERT_EQ(std::string("A 0\nAaaa 1\nBbbbb 2\nCcc 3\n"), avl.print());
 }
 
@@ -175,14 +148,14 @@ TEST(AVL, insert2) {
     std::string out;
     for (int i = 0; i < 100; ++i) {
         std::stringstream ss;
-        Data d;
+        AVLData d;
         char ch[i + 1];
         for (int j = 0; j < i; ++j) {
             ch[j] = (i + j) % 26 + 'a';
         }
         ch[i] = 0;
         d.name = std::string(ch);
-        d.val = i;
+        d.heap_pos = i;
         ss << d << '\n';
         out = ss.str();
         input.push_back(out);
@@ -198,21 +171,21 @@ TEST(AVL, insert2) {
 
 TEST(AVL, change) {
     AVL avl;
-    Data d1, d2, d3, d4, d5;
+    AVLData d1, d2, d3, d4, d5;
     d1.name = "aaa";
-    d1.val = 1;
+    d1.heap_pos = 1;
 
     d2.name = "bbb";
-    d2.val = 2;
+    d2.heap_pos = 2;
 
     d3.name = "ccc";
-    d3.val = 3;
+    d3.heap_pos = 3;
 
     d4.name = "ddd";
-    d4.val = 4;
+    d4.heap_pos = 4;
 
     d5.name = "eee";
-    d5.val = 5;
+    d5.heap_pos = 5;
 
     avl.insert(d1);
     avl.insert(d2);
@@ -223,15 +196,68 @@ TEST(AVL, change) {
     
     ASSERT_EQ(out, avl.print());
 
-    d2.val = 10;
+    d2.heap_pos = 10;
     avl.change(d2);
     
-    d3.val = 15;
+    d3.heap_pos = 15;
     avl.change(d3);
 
     out = "aaa 1\nbbb 10\nccc 15\nddd 4\neee 5\n";
     ASSERT_EQ(out, avl.print());
 }
+
+
+TEST(Heap, add) {
+    Heap heap;
+    HeapData d[4];
+    for (int i = 0; i < 4; ++i) {
+        d[i].priority = i + 1;
+    }
+
+    for (int i = 0; i < 4; ++i) {
+        heap.add(d[i]);
+        ASSERT_EQ(d[i].priority, heap.get_max().priority);
+        ASSERT_EQ(d[i].node, heap.get_max().node);
+    }
+    for (int i = 0; i < 4; ++i) {
+        ASSERT_EQ(d[3 - i].priority, heap.get_max().priority);
+        ASSERT_EQ(d[3 - i].node, heap.get_max().node);
+        heap.pop_max();
+    }
+    ASSERT_TRUE(heap.empty());
+
+    for (int i = 3; i >= 0; --i) {
+        heap.add(d[i]);
+        ASSERT_EQ(d[3].node, heap.get_max().node);
+        ASSERT_EQ(d[3].priority, heap.get_max().priority);
+    }
+    for (int i = 0; i < 4; ++i) {
+        ASSERT_EQ(d[3 - i].node, heap.get_max().node);
+        ASSERT_EQ(d[3 - i].priority, heap.get_max().priority);
+        heap.pop_max();
+    }
+    ASSERT_TRUE(heap.empty());
+}
+
+
+TEST(Heap, add2) {
+    Heap heap;
+    std::vector<HeapData> input;
+    std::string out;
+    for (int i = 0; i < 100; ++i) {
+        HeapData d;
+        d.priority = rand() % 1000;
+        input.push_back(d);
+        heap.add(input.back());
+    }
+    std::sort(input.rbegin(), input.rend());
+    for (auto it : input) {
+        ASSERT_EQ(it.node, heap.get_max().node);
+        ASSERT_EQ(it.priority, heap.get_max().priority);
+        heap.pop_max();
+    }
+}
+
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);    
